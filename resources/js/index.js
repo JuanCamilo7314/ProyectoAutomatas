4bedb1025b5cafb00da0e2ba07f6882fd4c083bf
Camilo Uni <juan.1701822418@ucaldas.edu.co>
2021-07-24 18:30:58 -0500

    se agrego algunas tablas y se termino la pagina inicial

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,7 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, {
-      indicators: true,
-      padding: 200,
-    });
+document.addEventListener('DOMContentLoaded', () => {
+  // initialize carousel
+  const carousel = document.querySelectorAll('.carousel');
+  M.Carousel.init(carousel, {
+    shift: -2,
+    indicators: true, // this option is require for autoplay functionnality
   });
+  
+  // custom function for autoplaying 
+  let indicatorItems = document.querySelectorAll('.carousel .indicator-item'),
+      slideTime = 3000,
+      activeClass = "active";
+
+  setInterval(() => {
+    indicatorItems.forEach(el => {
+      if (el.classList.contains(activeClass)) {
+        sib = el.nextElementSibling;
+        if (sib == null) {
+          indicatorItems[0].click();
+        } else {
+          sib.click()
+        }
+      }
+    });
+  }, slideTime);
+});
